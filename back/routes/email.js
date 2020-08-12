9ff5a46f683a13c3c7f0ffcfa8eae461dcaa1685
Votre Nom <Vous@exemple.com>
2020-08-12 21:15:54 +0200

    nodemailer finished

--- back/routes/email.js
+++ back/routes/email.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const router = express.Router()
 const transporter = require('../helper/transporter')
+const { connection } = require('../helper/config')
+
+router.get('/', (req, res) => {
+  const sql = 'SELECT * FROM recipient'
+  connection.query(sql, (err, result) => {
+    if(err) throw err
+    return res.status(200).send(result)
+  })
+
+})
 
 router.post('/', (req, res, next) => {
+  const gifts = req.body.gifts
+  const contactList = req.body.contact 
+  const nameList = gifts.map(gift => gift.name)
+  const contact = contactList.map(list => list.email)
   const mail = {
     from: process.env.THE_EMAIL,
     to: process.env.THE_EMAIL,
     subject: req.body.subject,
     text: `
-    from: ${req.body.name}
-    contact: ${req.body.email}
-    subject: ${req.body.subject}
-    message: ${req.body.text}`
+    from: Stéphane Pinto 
+    contact: ${contact}
+    subject: Gifts
+    message: Hello
+    here is my gift list
+    ${nameList}
+    cordially`
   }
 
   transporter.sendMail(mail, (err, data) => {
